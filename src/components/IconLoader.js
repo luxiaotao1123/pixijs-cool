7a26829dae548776a821f1c839d430ace2d4909e
luxiaotao1123 <t1341870251@163.com>
2023-04-17 16:52:31 +0800

    #

--- src/components/IconLoader.js
+++ src/components/IconLoader.js
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js';
-import * as TWEEDLE from 'tweedle.js'
+import * as TWEEDLE from 'tweedle.js';
+import axios from 'axios'
 
 class IconLoader {
 
@@ -10,8 +11,31 @@ class IconLoader {
     }
 
     load() {
-        PIXI.Assets.add('flowerTop', 'flowerTop.png');
-        PIXI.Assets.add('eggHead', 'eggHead.png');
+
+        this.requestAsset().then(res => {
+            for (let item of res.data.list) {
+                PIXI.Assets.add(item.name, item.src);
+            }
+            const texturesPromise = PIXI.Assets.backgroundLoad(res.data.list.map(e => {
+                return e.name;
+            }));
+            for (let item of res.data.list) {
+                PIXI.Assets.load(item.name).then((texture) => {
+                    const character = new PIXI.Sprite(texture);
+                    character.anchor.set(0.5);
+                    character.scale.set(0.5);
+                    character.x = this.#basketContainer.width / 2;
+                    character.y = this.#basketContainer.height / 2;
+                    character.interactive = true;
+                    character.cursor = 'pointer';
+                    this.#basketContainer.addChild(character);
+                });
+            }
+
+        })
+
+        // PIXI.Assets.add('flowerTop', 'flowerTop.png');
+        // PIXI.Assets.add('eggHead', 'eggHead.png');
 
         // const texturesPromise = PIXI.Assets.load(['flowerTop', 'eggHead']);
 
@@ -34,18 +58,22 @@ class IconLoader {
         // });
 
 
-        const texturesPromise = PIXI.Assets.backgroundLoad(['flowerTop', 'eggHead']);
-        PIXI.Assets.load('eggHead').then((texture) => {
-            // create a new Sprite from the resolved loaded texture
-            const character = new PIXI.Sprite(texture);
-            character.anchor.set(0.5);
-            character.scale.set(0.5);
-            character.x = this.#basketContainer.width / 2;
-            character.y = this.#basketContainer.height / 2;
-            character.interactive = true;
-            character.cursor = 'pointer';
-            this.#basketContainer.addChild(character);
-        });
+        // const texturesPromise = PIXI.Assets.backgroundLoad(['flowerTop', 'eggHead']);
+        // PIXI.Assets.load('eggHead').then((texture) => {
+        //     // create a new Sprite from the resolved loaded texture
+        //     const character = new PIXI.Sprite(texture);
+        //     character.anchor.set(0.5);
+        //     character.scale.set(0.5);
+        //     character.x = this.#basketContainer.width / 2;
+        //     character.y = this.#basketContainer.height / 2;
+        //     character.interactive = true;
+        //     character.cursor = 'pointer';
+        //     this.#basketContainer.addChild(character);
+        // });
+    }
+
+    async requestAsset() {
+        return await axios.get("asset.json");
     }
 
 }
