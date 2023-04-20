bd1be0655a0cb31d0b052909c5362ad4656e911c
luxiaotao1123 <t1341870251@163.com>
2023-04-20 11:22:24 +0800

    #

--- src/components/IconLoader.js
+++ src/components/IconLoader.js
@@ -31,6 +31,7 @@ class IconLoader {
                         character.anchor.set(0.5);
                         character.scale.set(0.2);
 
+                        // position
                         let row = Math.floor(i / columnNum);
                         let column = i % columnNum;
     
@@ -39,8 +40,50 @@ class IconLoader {
 
                         character.x = x - offset;
                         character.y = y;
+
+                        // action
                         character.interactive = true;
                         character.cursor = 'pointer';
+
+                        let backupCharacter = null;
+
+                        character.on('pointerdown', (event) => {
+                          backupCharacter = new PIXI.Sprite(texture);
+                          backupCharacter.anchor.set(0.5);
+                          backupCharacter.scale.set(0.2);
+                          backupCharacter.x = character.x;
+                          console.log(character.x);
+                          console.log(backupCharacter.x);
+                          backupCharacter.y = character.y;
+                          this.#basketContainer.addChild(backupCharacter);
+                          character.alpha = 0.5;
+                        });
+            
+                        character.on('pointermove', (event) => {
+                          if (backupCharacter) {
+                            console.log(event.data.global.x);
+                            backupCharacter.x = event.data.global.x;
+                            backupCharacter.y = event.data.global.y;
+                          }
+                        });
+            
+                        character.on('pointerup', (event) => {
+                          if (backupCharacter) {
+                            this.#basketContainer.removeChild(backupCharacter);
+                            backupCharacter = null;
+                            character.alpha = 1;
+                            if (this.#basketContainer.getBounds().contains(event.data.global.x, event.data.global.y)) {
+                              this.#basketContainer.addChild(character);
+                              character.x = event.data.global.x - this.#basketContainer.x;
+                              character.y = event.data.global.y - this.#basketContainer.y;
+                            } else {
+                              character.x = x - offset;
+                              character.y = y;
+                            }
+                          }
+                        });
+
+
                         this.#basketContainer.addChild(character);
                     });
                 }
