567fed94cf24d65bf759217e00b29da5ce0b099e
luxiaotao1123 <t1341870251@163.com>
2023-04-26 10:13:33 +0800

    #

---
+++ src/util/CoolGpt.js
@@ -0,0 +1,44 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+export default class CoolGpt {
+
+    static #organization = "org-GSDrqLKmjbrk4eLEw3UOq6XW";
+
+    static #apiKey = "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";
+
+    #config;
+    #openai;
+
+    static get openai() {
+        if (!CoolGpt.#config) {
+            CoolGpt.#config = new Configuration({
+                organization: CoolGpt.#organization,
+                apiKey: CoolGpt.#apiKey,
+            });
+            CoolGpt.#config.baseOptions.headers['User-Agent'] = 'CoolGpt/1.0';
+        }
+        if (!CoolGpt.#openai) {
+            CoolGpt.#openai = new OpenAIApi(CoolGpt.#config);
+        }
+        return CoolGpt.#openai;
+    }
+
+    static async ask(msg) {
+        const res = await CoolGpt.openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content: msg,
+                },
+            ],
+        });
+        console.log(res);
+        const choices = res?.data?.choices;
+        if (!choices || choices.length === 0) {
+            return null;
+        }
+        return choices.map((choice) => choice.message.content);
+    }
+
+}
